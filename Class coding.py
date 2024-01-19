0b5a57394c56187045f56a4640f000e0a978e6d6
simplicityiskey <123360358+simplicityiskey@users.noreply.github.com>
2024-01-19 07:04:44 +0000

    This is the true start
    I will crush this thing

---
+++ Class coding.py
@@ -0,0 +1,14 @@
+#Ask the user their name and thhen reply 'Hello and then "USER"'
+
+user = input('What is your name? ')
+print ('Hello', user)
+
+#Ex 3
+print('This code is used to calculate the area of the room')
+print('I am goinng to assume that you room is a rectangle')
+length = float(input('What is the length of the room in question? '))
+width = float(input('What is the wiidth of the room in question? '))
+area = length * width
+
+print ('The area of the room is', area, 'meters squared')
+
