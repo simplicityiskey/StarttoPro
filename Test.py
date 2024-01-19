65723a0713b52557bc4cae1737f51021335e82f2
simplicityiskey <123360358+simplicityiskey@users.noreply.github.com>
2024-01-19 06:35:01 +0000

    Test
    I was testing whether or not it would go to the branch

---
+++ Test.py
@@ -0,0 +1,13 @@
+count = 0
+
+while count < 30:
+#    print('I am counting') 
+    
+#    if (count%2==0):
+#        print('This number is also even')
+#    else:
+#        print('keep going')
+    print(count)
+
+    
+    count += 1
